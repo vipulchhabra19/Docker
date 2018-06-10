7fc7612406558585656b0e3c346d73759ff41360
Vipulchhabra19 <vipulchhabrahmo@gmail.com>
2018-06-10 11:41:30 +0530

    Docker compose setup

--- app.py
+++ app.py
@@ -21,5 +21,6 @@ def hello():
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+	
+    app.run(debug=True,host='0.0.0.0')
 
